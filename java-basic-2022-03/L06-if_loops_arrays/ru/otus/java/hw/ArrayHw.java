dc0c315a0216e6113f66208b3eb323a783d70b87
ciscomsk <kuznetzoff@gmail.com>
2022-04-26 11:57:41 +0300

    Add solution with 1 three-dimensional array

--- java-basic-2022-03/L06-if_loops_arrays/ru/otus/java/hw/ArrayHw.java
+++ java-basic-2022-03/L06-if_loops_arrays/ru/otus/java/hw/ArrayHw.java
@@ -51,53 +51,112 @@ public class ArrayHw {
 //        }
 
         // v2 - multiple arrays
-        String[] questions = {
-                "Если тип данных является монадой - справедливо ли утверждение, что он является функтором?",
-                "Выберете правильную сигнатуру метода FlatMap в Scala",
-                "В Scala тип данных Future является не ленивым(eager) по умолчанию - возможно ли сделать его ленивым(lazy)?"
-        };
+//        String[] questions = {
+//                "Если тип данных является монадой - справедливо ли утверждение, что он является функтором?",
+//                "Выберете правильную сигнатуру метода FlatMap в Scala",
+//                "В Scala тип данных Future является не ленивым(eager) по умолчанию - возможно ли сделать его ленивым(lazy)?"
+//        };
+//
+//        String[][] answerOptions = {
+//                {
+//                        "да",
+//                        "нет",
+//                        "возможно"
+//                },
+//                {
+//                        "flatMap[B](f: A => B): F[B]",
+//                        "flatMap[B](f: A => F[B]): F[B]",
+//                        "flatmap[B](f: A => Unit): Unit"
+//                },
+//                {
+//                        "нет",
+//                        "да - с помощью () => Future",
+//                        "да - при объявлении его в for-comprehension",
+//                        "верны варианты 2 и 3",
+//                }
+//        };
+//
+//        String[] correctAnswers = {"1", "2", "4"};
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        for (int i = 0; i < questions.length; i++) {
+//            System.out.println(questions[i]);
+//            for (int j = 0; j <answerOptions[i].length; j++) {
+//                System.out.println((j + 1) + ". " + answerOptions[i][j]);
+//            }
+//
+//            System.out.println("Ваш ответ: ");
+//            String answer = sc.nextLine().trim();
+//
+//            if (answer.equals(correctAnswers[i])) {
+//                System.out.println("Правильно");
+//                rightAns++;
+//            } else {
+//                System.out.println("Неправильно");
+//                wrongAns++;
+//            }
+//
+//            System.out.println();
+//        }
 
-        String[][] answerOptions = {
+        // v3 - 1 three-dimensional array
+        String[][][] pollData =
                 {
-                        "да",
-                        "нет",
-                        "возможно"
-                },
-                {
-                        "flatMap[B](f: A => B): F[B]",
-                        "flatMap[B](f: A => F[B]): F[B]",
-                        "flatmap[B](f: A => Unit): Unit"
-                },
-                {
-                        "нет",
-                        "да - с помощью () => Future",
-                        "да - при объявлении его в for-comprehension",
-                        "верны варианты 2 и 3",
-                }
-        };
-
-        String[] correctAnswers = {"1", "2", "4"};
+                        {
+                                {"Если тип данных является монадой - справедливо ли утверждение, что он является функтором?"},
+                                {
+                                        "да",
+                                        "нет",
+                                        "возможно"
+                                },
+                                {"1"}
+                        },
+                        {
+                                {"Выберете правильную сигнатуру метода FlatMap в Scala"},
+                                {
+                                        "flatMap[B](f: A => B): F[B]",
+                                        "flatMap[B](f: A => F[B]): F[B]",
+                                        "flatmap[B](f: A => Unit): Unit"
+                                },
+                                {"2"}
+                        },
+                        {
+                                {"В Scala тип данных Future является не ленивым(eager) по умолчанию - возможно ли сделать его ленивым(lazy)?"},
+                                {
+                                        "нет",
+                                        "да - с помощью () => Future",
+                                        "да - при объявлении его в for-comprehension",
+                                        "верны варианты 2 и 3"
+                                },
+                                {"4"}
+                        }
+                };
 
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println(questions[i]);
-            for (int j = 0; j <answerOptions[i].length; j++) {
-                System.out.println((j + 1) + ". " + answerOptions[i][j]);
+        for (String[][] question : pollData) {
+            for (int i = 0; i < question.length - 1; i++) {
+                for (int j = 0; j < question[i].length; j++) {
+
+                    if (i == 0) System.out.println(question[i][j]);
+                    else System.out.println((j + 1) + ". " + question[i][j]);
+
+                    if (i == 1 && j == question[i].length - 1) {
+                        System.out.println("Ваш ответ: ");
+                        String answer = sc.nextLine().trim();
+
+                        if (answer.equals(question[2][0])) {
+                            System.out.println("Правильно");
+                            rightAns++;
+                        } else {
+                            System.out.println("Неправильно");
+                            wrongAns++;
+                        }
+                        System.out.println();
+                    }
+                }
             }
-
-            System.out.println("Ваш ответ: ");
-            String answer = sc.nextLine().trim();
-
-            if (answer.equals(correctAnswers[i])) {
-                System.out.println("Правильно");
-                rightAns++;
-            } else {
-                System.out.println("Неправильно");
-                wrongAns++;
-            }
-
-            System.out.println();
         }
 
         System.out.println("Результат: правильно " + rightAns + ", неправильно " + wrongAns);

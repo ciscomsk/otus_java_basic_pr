59634cb4998e5485536523a72efafc42d2789173
ciscomsk <kuznetzoff@gmail.com>
2022-06-13 18:39:14 +0300

    Fix review comments

--- java-basic-2022-03/L10-collections/src/main/java/hw/v2/Bank.java
+++ java-basic-2022-03/L10-collections/src/main/java/hw/v2/Bank.java
@@ -1,40 +1,21 @@
 package hw.v2;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class Bank {
     private Map<Client, List<Account>> clients;
     private Map<Account, Client> accounts;
-
-    public Bank(
-            Map<Client, List<Account>> clients,
-            Map<Account, Client> accounts
-            ) {
-        this.clients = clients;
-        this.accounts = accounts;
-    }
-
-    public Map<Client, List<Account>> getClients() {
-        return clients;
-    }
-
-    public Map<Account, Client> getAccounts() {
-        return accounts;
-    }
-
-    public void setClients(Map<Client, List<Account>> clients) {
-        this.clients = clients;
-    }
-
-    public void setAccounts(Map<Account, Client> accounts) {
-        this.accounts = accounts;
-    }
-
     public Client findClient(Account account) {
         return accounts.get(account);
     }
-
     public List<Account> getClientAccounts(Client client) {
         return clients.get(client);
     }

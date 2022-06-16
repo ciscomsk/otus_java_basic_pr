37857261447d8d5b11cf40474c10c9bf5a86bdd6
ciscomsk <kuznetzoff@gmail.com>
2022-06-16 16:50:57 +0300

    Fix review comments

--- java-basic-2022-03/L10-collections/src/main/java/hw/Playground.java
+++ java-basic-2022-03/L10-collections/src/main/java/hw/Playground.java
@@ -1,31 +1,55 @@
 package hw;
 
-
 import java.util.*;
 
 public class Playground {
     public static void main(String[] args) {
-//        List<Integer> manualList = new ArrayList<>(Arrays.asList(1, -5, 20, 4));
+        List<Integer> manualList = new ArrayList<>(Arrays.asList(1, -5, 20, 4));
 //        HandmadeSelectionSort.sort(manualList);
 //        System.out.println(manualList);
 
-        var generatedList = generateRandomList(100_000);
-        System.out.println(generatedList);
+        var generatedList = generateRandomList(10_000);
+//        System.out.println(generatedList);
 
-        Long start = System.currentTimeMillis();
-        Collections.sort(generatedList);
-        HandmadeSelectionSort.sort(generatedList);
-        Long end = System.currentTimeMillis();
-        System.out.println("Sort time : " + (end - start));
-
+        System.out.println(getExecutionTime(() ->  Collections.sort(generatedList)));
+        System.out.println(getExecutionTime(() ->  HandmadeSelectionSort.sort(generatedList)));
+        System.out.println();
         /*
             10_000
-            Collections.sort == 6
-            HandmadeSelectionSort.sort == 116
+            Collections.sort            == 7
+            HandmadeSelectionSort.sort  == 78
 
             100_000
-            Collections.sort == 34
-            HandmadeSelectionSort.sort == 10491
+            Collections.sort            == 34
+            HandmadeSelectionSort.sort  == 7848
+         */
+
+        // err - ConcurrentModificationException
+//        for (int elem : manualList) {
+//            System.out.println(elem);
+//            manualList.add(7);
+//        }
+
+        var emptyLL = new LinkedList<Integer>();
+        var emptyAL = new ArrayList<Integer>();
+
+        var insertLLTime = getExecutionTime(() -> {
+            for (int i = 0; i <= 100_000_000; i++) {
+                emptyLL.add(i);
+            }
+        });
+        System.out.println(insertLLTime); //
+
+        var insertALTime = getExecutionTime(() -> {
+            for (int i = 0; i <= 100_000_000; i++) {
+                emptyAL.add(i);
+            }
+        });
+        System.out.println(insertALTime);
+
+        /*
+            Linked list == 13646
+            Array list  == 3639
          */
     }
 
@@ -42,4 +66,13 @@ public class Playground {
 
         return list;
     }
+
+    public static long getExecutionTime(Runnable code) {
+        Long start = System.currentTimeMillis();
+        code.run();
+        Long end = System.currentTimeMillis();
+
+        return end - start;
+    }
+
 }
